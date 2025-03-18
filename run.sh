46f6b33c7bd45fd344c92af8003614cdc0cad820
kauan resende <kauan26fr@outlook.com>
2025-03-18 10:51:15 -0300

    18.03 Terca-feira 10:51

---
+++ run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USER=$(logname)
+MONITOR_SERVICE="/etc/systemd/system/monitor.service"
+MONITOR_SCRIPT_PATH="/opt/BalancaPubRepo/bin/_monitor.sh"
+
+sudo chmod +x "$MONITOR_SCRIPT_PATH"
+
+sudo tee "$MONITOR_SERVICE" > /dev/null <<EOF
+[Unit]
+Description=Iniciar o aplicativo no boot do sistema (root)
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash $MONITOR_SCRIPT_PATH
+WorkingDirectory=/opt/BalancaPubRepo/bin
+Restart=always
+User=root
+StandardOutput=append:/tmp/monitor.log
+StandardError=append:/tmp/monitor_error.log
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+WIFICONFIG_SERVICE="/etc/systemd/system/wifi-setup-monitor.service"
+WIFICONFIG_SCRIPT_PATH="/opt/BalancaPubRepo/bin/_wificonfig.sh"
+
+sudo chmod +x "$WIFICONFIG_SCRIPT_PATH"
+
+sudo tee "$WIFICONFIG_SERVICE" > /dev/null <<EOF
+[Unit]
+Description=Monitoramento de USB para Configuração de Wi-Fi
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash $WIFICONFIG_SCRIPT_PATH
+Restart=always
+User=root
+StandardOutput=append:/tmp/usb_wifi_monitor.log
+StandardError=append:/tmp/usb_wifi_monitor_error.log
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable monitor.service
+sudo systemctl start monitor.service
+sudo systemctl enable wifi-setup-monitor.service
+sudo systemctl start wifi-setup-monitor.service
+
+echo "✅Serviço _monitor.* criado e iniciado com sucesso!"
+echo "✅Serviço _wifi-setup-monitor.* criado e iniciado com sucesso!"
