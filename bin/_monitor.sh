a5a41a2581e0de71d4ce91fb4c17eb3e07bb05ce
kauan resende <kauan26fr@outlook.com>
2025-03-18 13:37:38 -0300

    18.03 Terca-feira 13:37

--- bin/_monitor.sh
+++ bin/_monitor.sh
@@ -18,7 +18,6 @@ sudo git -C $REPO_PATH reset --hard origin/main
 
 if ! pgrep -f "python3 $REPO_PATH/bin/app/index.py" > /dev/null; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Iniciando aplicação..." | tee -a /tmp/monitor.log
-    sudo $CMD_INICIAR
 else
     echo "$(date '+%Y-%m-%d %H:%M:%S') - A aplicação já está rodando." | tee -a /tmp/monitor.log
 fi
