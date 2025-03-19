14460ef8a2070fcd803f00f1760032f688054733
kauan resende <kauan26fr@outlook.com>
2025-03-19 13:32:08 -0300

    19.03 Quarta-feira 13:32

--- bin/_wificonfig.sh
+++ bin/_wificonfig.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 MOUNT_PATH="/mnt/usb_wifi"
-[ ! -d "$MOUNT_PATH" ] && mkdir -p "$MOUNT_PATH"
 
-echo "Dispositivo detectado: $1" | logger
+DEVICE=$(echo "$1" | sed 's/[0-9]*$//')
+
+for PARTITION in /dev/${DEVICE}*; do
+    if mount | grep -q "$PARTITION"; then
+        echo "$PARTITION já montado." | logger
+        continue
+    fi
+
+    if grep -q "wifi_setup.txt" <(ls "$PARTITION"/*); then
+        [ ! -d "$MOUNT_PATH" ] && mkdir -p "$MOUNT_PATH"
+        mount "$PARTITION" "$MOUNT_PATH" || exit 1
+
+        echo "Configurando Wi-Fi a partir de: $PARTITION" | logger
+
+        umount "$MOUNT_PATH"
+        break
+    fi
+done
