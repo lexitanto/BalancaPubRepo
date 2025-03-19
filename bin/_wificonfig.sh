ce4dbed17f6fa9a4e7d472e4b5a2f4305dcf3d36
kauan resende <kauan26fr@outlook.com>
2025-03-19 13:14:58 -0300

    19.03 Quarta-feira 13:14

--- bin/_wificonfig.sh
+++ bin/_wificonfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MOUNT_PATH="/mnt/usb_wifi"
-[ ! -d "$MOUNT_POINT" ] && mkdir -p "$MOUNT_POINT"
+[ ! -d "$MOUNT_PATH" ] && mkdir -p "$MOUNT_PATH"
 
 echo "Dispositivo detectado: $1" | logger
