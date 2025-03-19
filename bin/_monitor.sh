31701f8aab76349a8b36132f8f7586faf74ebe6b
kauan resende <kauan26fr@outlook.com>
2025-03-19 10:21:56 -0300

    19.03 Quarta-feira 10:21

--- bin/_monitor.sh
+++ bin/_monitor.sh
@@ -3,18 +3,13 @@
 REPO_PATH="/opt/BalancaPubRepo"
 CMD_INICIAR="python3 $REPO_PATH/bin/app/index.py"
 
-until ping -c 1 github.com &>/dev/null; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Esperando por conexão com a internet..." | tee -a /tmp/monitor.log
-    sleep 5
-done
-
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Verificando e atualizando o repositório..." | tee -a /tmp/monitor.log
 
 # Atualiza o repositório
-sudo git -C $REPO_PATH fetch origin
+git -C $REPO_PATH fetch origin
 
 # Força a atualização do repositório local para o estado remoto, ignorando quaisquer mudanças locais
-sudo git -C $REPO_PATH reset --hard origin/main
+git -C $REPO_PATH reset --hard origin/main
 
 if ! pgrep -f "python3 $REPO_PATH/bin/app/index.py" > /dev/null; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Iniciando aplicação..." | tee -a /tmp/monitor.log
