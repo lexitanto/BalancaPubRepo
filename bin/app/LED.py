24d90667d79b6d29b2346b58dba080ceaa708f58
kauan resende <kauan26fr@outlook.com>
2025-03-20 17:50:06 -0300

    20.03 Quinta-feira 17:50

--- bin/app/LED.py
+++ bin/app/LED.py
@@ -1,52 +1,44 @@
 import RPi.GPIO as GPIO
 import time
-import sys
+import threading
 
 class LEDController:
     def __init__(self, pin=21):
         self.led_pin = pin
+        self.blinking = False
+        self.thread = None
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.led_pin, GPIO.OUT)
-    
-    def piscar_led(self, intervalo=1):
-        """Pisca o LED em intervalos definidos."""
-        try:
-            while True:
-                GPIO.output(self.led_pin, GPIO.HIGH)
-                time.sleep(intervalo)
-                GPIO.output(self.led_pin, GPIO.LOW)
-                time.sleep(intervalo)
-        except KeyboardInterrupt:
-            print("\nInterrompido pelo usuário. Limpando GPIO...")
-        finally:
-            self.cleanup()
-    
-    def acender_led(self):
-        """Liga o LED."""
-        GPIO.output(self.led_pin, GPIO.HIGH)
-    
-    def desligar_led(self):
-        """Desliga o LED."""
-        GPIO.output(self.led_pin, GPIO.LOW)
-    
-    def cleanup(self):
-        """Libera os recursos do GPIO."""
-        GPIO.cleanup()
-        print("GPIO liberado.")
 
-# Inicia o controle do LED
-if __name__ == "__main__":
-    controller = LEDController()
-    try:
-        if len(sys.argv) > 1 and sys.argv[1] == "piscar":
-            controller.piscar_led()  # Começa a piscar o LED
-        else:
-            controller.acender_led()  # Mantém o LED aceso
-            print("Pressione Ctrl+C para sair.")
-            while True:
-                time.sleep(1)  # Mantém o programa rodando
-    except KeyboardInterrupt:
-        print("\nSaindo...")
-    finally:
-        controller.cleanup()
+    def _piscar_led(self, intervalo=1):
+        while self.blinking:
+            GPIO.output(self.led_pin, GPIO.HIGH)
+            time.sleep(intervalo)
+            GPIO.output(self.led_pin, GPIO.LOW)
+            time.sleep(intervalo)
+
+    def piscar_led(self, intervalo=1):
+        if not self.blinking:
+            self.blinking = True
+            self.thread = threading.Thread(target=self._piscar_led, args=(intervalo,), daemon=True)
+            self.thread.start()
+
+    def parar_led(self):
+        self.blinking = False
+        if self.thread:
+            self.thread.join(timeout=1)
+        GPIO.output(self.led_pin, GPIO.LOW)
+
+    def acender_led(self):
+        self.parar_led()
+        GPIO.output(self.led_pin, GPIO.HIGH)
+
+    def desligar_led(self):
+        self.parar_led()
+        GPIO.output(self.led_pin, GPIO.LOW)
+
+    def cleanup(self):
+        self.parar_led()
+        GPIO.cleanup()
