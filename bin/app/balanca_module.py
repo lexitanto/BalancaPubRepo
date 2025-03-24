e618a709b0a93d1f4bf912dd4e688d510c140dc5
kauan resende <kauan26fr@outlook.com>
2025-03-24 10:31:04 -0300

    24.03 Segunda-feira  10:31

--- bin/app/balanca_module.py
+++ bin/app/balanca_module.py
@@ -78,11 +78,11 @@ class balanca():
             return None
 
     def find_and_open_serial(self):
-        LED_CONTROL._piscar_led(LED_CONTROL.led_config_ext)
+        LED_CONTROL.start_blinking(LED_CONTROL.led_config_ext, intervalo=1)
         while True:
             dispositivo = self.find_prolific()
             if dispositivo:
-                LED_CONTROL.desligar_led(LED_CONTROL.led_config_ext)
+                LED_CONTROL.stop_blinking(LED_CONTROL.led_config_ext)
                 try:
                     self.serial_port.port = dispositivo
                     self.serial_port.baudrate = 9600
